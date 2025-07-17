3f0e391772bdc8ea785a1470dda2d3e8b837ae01
Promesse RUKUNDO <130702756+Promesse7@users.noreply.github.com>
2025-07-17 17:29:42 +0200

    resolved the init error

--- U-net-best.py
+++ U-net-best.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels, mid_channels=None):
-        super()__init__()
+        super().__init__()
         if not mid_channels:
             mid_channels = out_channels
         self.double_conv = nn.Sequential(
